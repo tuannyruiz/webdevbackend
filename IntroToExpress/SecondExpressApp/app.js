49573e5adc010eaefa098ea4c8af0fe78fb6680b
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-22 00:07:37 +0000

    lowercase the input animal

--- IntroToExpress/SecondExpressApp/app.js
+++ IntroToExpress/SecondExpressApp/app.js
@@ -6,7 +6,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/speak/:animal", function(req, res) {
-    var animal = req.params.animal;
+    var animal = req.params.animal.toLowerCase();
     var sounds = {
         pig: "oink",
         cow: "moo",
