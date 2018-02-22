bcf7a74155d7a84ab8562082ca0ef070ef4624fc
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-22 00:05:45 +0000

    learning dictionary

--- IntroToExpress/SecondExpressApp/app.js
+++ IntroToExpress/SecondExpressApp/app.js
@@ -7,10 +7,21 @@ app.get("/", function(req, res){
 
 app.get("/speak/:animal", function(req, res) {
     var animal = req.params.animal;
-    res.send("The " + animal +  " says: ")
+    var sounds = {
+        pig: "oink",
+        cow: "moo",
+        dog: "woof woof",
+        goldfish: "..."
+    }
+    var sound = sounds[animal];
+    res.send("The " + animal +  " says: " + sound)
 })
 
-
+app.get("/repeat/:word/:times", function(req, res) {
+    var word = req.params.word;
+    var times = req.params.times;
+    res.send((word + ' ').repeat(times));
+})
 
 app.get("*", function(req, res){
     res.send("Sorry, page not found!")
