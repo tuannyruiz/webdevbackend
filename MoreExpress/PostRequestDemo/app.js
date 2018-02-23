efcdfccb1785bfb9c255d2304c5344600e02e3c3
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-23 19:11:32 +0000

    learning post requests

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -8,7 +8,12 @@ app.get("/", function(req, res) {
 });
 
 app.get("/friends", function(req, res) {
-    res.render("friends");
+    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends", {friends: friends}); // The second "friends" is the var declared in this app.get
+});
+
+app.post("/addfriend", function(req, res) {
+    res.send("You have reached the post route");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
