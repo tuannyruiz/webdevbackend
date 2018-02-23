51ce1c7a31ecf079568f065ae463d0ae636391ea
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-23 17:54:31 +0000

    adding styles

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
     res.render("home.ejs");
