c12b5cd28292ff2f5332bfcc1b5162cd87e23c4c
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-23 18:49:35 +0000

    start a new project

---
+++ MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/friends", function(req, res) {
+    res.render("friends");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server listening!");
+});
