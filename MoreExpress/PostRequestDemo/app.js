08679551b6a6013496b29f2a45660a53e2930586
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-02-25 21:47:56 +0000

    adding a new friend in the array and showing it

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -1,18 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get("/", function(req, res) {
     res.render("home");
 });
 
 app.get("/friends", function(req, res) {
-    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
     res.render("friends", {friends: friends}); // The second "friends" is the var declared in this app.get
 });
 
 app.post("/addfriend", function(req, res) {
+   var newFriend = req.body.newfriend; //newfriend is the 'name' of the input we're getting the data from
+   friends.push(newFriend);
     res.send("You have reached the post route");
 });
 
